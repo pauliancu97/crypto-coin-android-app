bca388a3d31c79602baf1e46955f8e7a4a3bad23
paul97iancu <paul97iancu@gmail.com>
2020-08-09 12:09:09 +0300

    Initial commit

---
+++ app/src/main/java/com/paul/android/cryptocoinandroidapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.paul.android.cryptocoinandroidapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
